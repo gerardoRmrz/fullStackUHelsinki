732b39f1b1fb350438d1c5c222f89153acdcd008
gerardoRmrz <grmzrso@outlook.com>
2026-01-11 19:42:49 -0500

    Se añadieron middlewares para logeo

--- part4/app.js
+++ part4/app.js
@@ -4,24 +4,29 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/appRouter')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 
 mongoose.set('strictQuery', false)
 
-console.log('Connecting to', config.MONGODB_URI)
+logger.info('Connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then( () => {
-    console.log('connected to MONGO DB')
+    logger.info('connected to MONGO DB')
   } )
 
   app.use(cors())
   app.use(express.json())
+  app.use(middleware.requestLogger)
   app.use('/api/users', usersRouter)
   app.use('/api/blogs', blogsRouter)
   app.use('/api/login', loginRouter)
 
+  app.use(middleware.unknownEndpoint)
+  app.use(middleware.errorHandler)
   module.exports = app
 
