044966a72f61c565b5a737decdc59fedb9d790b1
gerardoRmrz <grmzrso@outlook.com>
2026-04-20 11:13:04 -0500

    La lista de libros y autores se actualiza cuendo se agrega un nuevo libro

--- part8/client/src/components/NewBook.jsx
+++ part8/client/src/components/NewBook.jsx
@@ -10,7 +10,7 @@ const NewBook = () => {
   const [genres, setGenres] = useState([]);
 
   const [addBook] = useMutation(ADD_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }], // actualiza la lista de libros y autores
     onError: (error) => {
       const errors = error.errors[0];
       const messages = errors.message;
